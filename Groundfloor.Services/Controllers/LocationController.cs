4dce676a03fc208e91f6422e3d427905fc28c89c
Keith Harris <keith9820@hotmail.com>
2013-03-22 15:03:42 -0400

    moving files from trunk to root

---
+++ Groundfloor.Services/Controllers/LocationController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using GroundFloor.Services.Models;
+using MvcApplication1.Models;
+
+namespace GroundFloor.Services.Controllers
+{
+    public class LocationController : Controller
+    {
+        public ActionResult GeocodeZip(string zipcode)
+        {
+            var context = new GeoLocationEntities();
+            var loc = (from USCity c in context.USCity
+                       let zip = context.USCity.Select(x => c.Zipcode).Cast<string>().FirstOrDefault()
+                       where zip == zipcode
+                       select c).FirstOrDefault();
+
+            if (loc != null)
+            {
+                return Json(new Location { City = loc.City, State = loc.State, Latitude = loc.Lat.ToString(), Longitude = loc.Lon.ToString(), Zipcode = zipcode }, "text/plain", JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new Location(), "text/plain", JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult GeocodeCity(string city, string state)
+        {
+            var context = new GeoLocationEntities();
+            var loc = context.USCities.Where(c => c.City.Equals(city, StringComparison.InvariantCultureIgnoreCase) && c.Region.Equals(state, StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
+
+            if (loc != null)
+            {
+                return Json(new Location { City = city, State = state, Latitude = loc.Latitude.GetValueOrDefault().ToString(), Longitude = loc.Longitude.GetValueOrDefault().ToString()}, "text/plain", JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new Location(), "text/plain", JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult ReverseGeocode(string latitude, string longitude)
+        {
+            var context = new GeoLocationEntities();
+            //todo:  calculate nearest city
+            var loc = context.USCities.Where(c => c.Latitude.ToString() == latitude && c.Longitude.ToString() == longitude).FirstOrDefault();
+
+            if (loc != null)
+            {
+                return Json(new Location { City = loc.City, State = loc.Region, Latitude = latitude, Longitude = longitude }, "text/plain", JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new Location(), "text/plain", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Groundfloor.Services/Controllers/SecurityController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Configuration;
+using System.Web.Mvc;
+using Groundfloor.Security;
+
+namespace GroundFloor.Services.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public ActionResult Encrypt(string data, string secret, bool urlencode = true)
+        {
+            var result = Encryptor.Encrypt64(data, secret);
+
+            if (urlencode)
+                result = result.EncodeUrl();
+
+            return Json(result, "text/plain", JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Decrypt(string data, string secret, bool urldecode = true)
+        {
+            if (urldecode)
+                data = data.DecodeUrl();
+
+            return Json(data.Decrypt64(secret), "text/plain", JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetToken(bool urlencode = true)
+        {
+            string secret = ConfigurationManager.AppSettings["AppSecret"];
+            string data = DateTime.Now.ToUniversalTime().ToString();
+            return Json(Encrypt(data, secret, urlencode), "text/plain", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
